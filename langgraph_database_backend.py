8f8309e6cc142d7603fc448ce31aa36609f4b7ae
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-07-14 12:44:54 +0530

    implemented database in backend

---
+++ langgraph_database_backend.py
@@ -0,0 +1,64 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+# from langgraph.checkpoint.sqlite import Sqlite
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+import sqlite3
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct", task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+# define state
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+# create a chat_node working function or logic
+def chat_node(state: ChatState):
+    # take user query from state
+    messages = state["messages"]
+
+    # send to llm
+    response = model.invoke(messages)
+
+    # response store state
+    return {"messages": [response]}
+
+
+# sqlite connections
+conn = sqlite3.connect(database="chatbot.db", check_same_thread=False)
+# add checkpoint
+checkpointer = SqliteSaver(conn=conn)
+
+# define graph
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node("chat_node", chat_node)
+
+# add edges
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+CONFIG = {"configurable": {"thread_id": "thread-1"}}
+
+response = chatbot.invoke(
+    {"messages": [HumanMessage(content="can you tell me my name")]},
+    config=CONFIG,
+)
+
+print(response)
