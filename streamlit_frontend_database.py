f5fb5a2b94e6b937c5a76881b24e0dff20bc927c
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-07-24 18:26:19 +0530

    added metadata and tags to trace project with langsmith implementation

--- streamlit_frontend_database.py
+++ streamlit_frontend_database.py
@@ -84,7 +84,11 @@ if user_input:
     with st.chat_message("user"):
         st.text(user_input)
 
-    CONFIG = {"configurable": {"thread_id": st.session_state["thread_id"]}}
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {"thread_id": st.session_state["thread_id"]},
+        "run_name": "chat_turn",
+    }
 
     # first add the message to the message history
     with st.chat_message("assistant"):
